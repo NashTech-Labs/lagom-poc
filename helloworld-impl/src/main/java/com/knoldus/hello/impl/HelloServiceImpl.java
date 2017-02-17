2acd898d0421a2f27b59248bf277938d3bed49ea
Himani Arora <1himani.arora@gmail.com>
2017-02-17 14:59:12 +0530

    Logging added for hello service

--- helloworld-impl/src/main/java/com/knoldus/hello/impl/HelloServiceImpl.java
+++ helloworld-impl/src/main/java/com/knoldus/hello/impl/HelloServiceImpl.java
@@ -13,7 +13,8 @@ import com.knoldus.hello.api.HelloService;
 import com.knoldus.hello.impl.HelloCommand.Hello;
 import com.knoldus.hello.impl.HelloCommand.UseGreetingMessage;
 import com.knoldus.hello.api.GreetingMessage;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import javax.inject.Inject;
 import java.util.Optional;
 
@@ -21,6 +22,8 @@ import java.util.Optional;
  * Implementation of the HelloService.
  */
 public class HelloServiceImpl implements HelloService {
+
+  private static final Logger log = LogManager.getLogger(HelloServiceImpl.class);
 
   private final PersistentEntityRegistry persistentEntityRegistry;
 
@@ -31,6 +34,10 @@ public class HelloServiceImpl implements HelloService {
 
   @Override public ServiceCall<NotUsed, String> hello(String id) {
     return request -> {
+      log.warn("hello service is called ");
+      log.info("hello service is called ");
+      log.debug("hello service is called ");
+      log.error("hello service is called ");
       PersistentEntityRef<HelloCommand> ref = persistentEntityRegistry.refFor(HelloWorld.class, id);
       return ref.ask(new Hello(id, Optional.empty()));
     };
